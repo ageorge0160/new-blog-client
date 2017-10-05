5e54eb6d6cacbb6b69b439d73939bb18e2b82767
Amanda George <github email address>
2017-10-05 18:16:02 -0500

    fix posts layout

--- src/containers/Posts.js
+++ src/containers/Posts.js
@@ -1,16 +1,14 @@
 import React from 'react'
 
-
-const Posts = ({posts}) => {
-  const renderPosts = posts.map((post) =>
-  <h6>{post.title}</h6>
-)
-
-return (
-  <div>
+const Posts = (props) => {
+  <div className="postContainer">
     <h1>Posts</h1>
-    {renderPosts}
+    {props.posts.map(post =>
+      <div key={post.id} className="postBox">
+        <h3>{post.title}</h3>
+        <p>{post.content}</p>
+      </div>
+    )}
   </div>
-)
-}
+};
 export default Posts;
