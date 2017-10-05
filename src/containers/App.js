fd8a9cdfd6ee99a95002afbd88305e413724a302
Amanda George <github email address>
2017-10-05 18:10:07 -0500

    add rest of app

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import Posts from './Posts'
 
-class App extends Component {
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/posts')
+    .then(response => response.json())
+    .then(posts => this.setState({ posts }))
+  }
+
   render() {
     return (
       <div className="App">
         <Posts posts={this.state.posts} />
       </div>
-    );
+    )
   }
 }
 
